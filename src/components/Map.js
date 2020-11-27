89ab1d76c4e2cfa85fc479a9d9c1ffce58aa3084
Trung Trinh <tqtrung83@gmail.com>
2020-11-27 00:26:33 -0330

    Added Location Info Box CSS

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import LocationMaker from './LocationMaker';
+import LocationInfoBox from './LocationInfoBox';
 
 const Map = ({ center, zoom, eventData }) => {
+  const [locationInfo, setLocationInfo] = useState(null);
+
   const markers = eventData.map((ev) => {
     if (ev.categories[0].id === 8) {
       return (
@@ -9,6 +13,7 @@ const Map = ({ center, zoom, eventData }) => {
           key={ev.id}
           lat={ev.geometries[0].coordinates[1]}
           lng={ev.geometries[0].coordinates[0]}
+          onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}
         />
       );
     }
@@ -24,6 +29,8 @@ const Map = ({ center, zoom, eventData }) => {
       >
         {markers}
       </GoogleMapReact>
+
+      {locationInfo && <LocationInfoBox info={locationInfo} />}
     </div>
   );
 };
