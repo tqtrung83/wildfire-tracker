fa078d2a404cab3ca4e3386134b8eedfa6295475
Trung Trinh <tqtrung83@gmail.com>
2020-11-27 00:08:10 -0330

    Added Location Info Box

---
+++ src/components/LocationInfoBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const LocationInfoBox = ({ info }) => {
+  return (
+    <div className='location-info'>
+      <h2>Event Location Info</h2>
+      <ul>
+        <li>
+          ID: <strong>{info.id}</strong>
+        </li>
+        <li>TITLE: {info.title}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default LocationInfoBox;
